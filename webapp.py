b8268575b97caa3d029343eaf6d2d4d09bbc14ab
MelanieCampuzano <undepth71@gmail.com>
2026-01-16 11:36:22 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,7 +11,7 @@ import sys
  
 app = Flask(__name__)
 
-app.debug = False #Change this to False for production
+app.debug = True #Change this to False for production
 #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #Remove once done debugging
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
